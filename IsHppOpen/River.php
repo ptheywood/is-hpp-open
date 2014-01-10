28aaec7bce9dbc659c3a2ae4e0d76e011e83becb
Peter Heywood <peethwd@gmail.com>
2014-01-10 01:25:55 +0000

    Improve documentation for IsHppOpen/River.php

--- IsHppOpen/River.php
+++ IsHppOpen/River.php
@@ -10,10 +10,20 @@ class River {
     private $uuid, $url, $river, $state;
     private $eaLevel;
 
+    /**
+     * Constructor allowing simple loading from a json string 
+     * @param string $json
+     * @author Peter Heywood <peethwd@gmail.com>
+     */
     function __construct($json = null){
         $this->loadFromJson($json);
     }
 
+    /**
+     * Populate instance variables based on json object from api.
+     * @param  string $json
+     * @author Peter Heywood <peethwd@gmail.com>
+     */
     private function loadFromJson($json){
         if(isset($json["uuid"])){
             $this->uuid = $json["uuid"];
@@ -33,29 +43,64 @@ class River {
 
     }
 
+    /**
+     * Getter method for uuid class variable
+     * @return string
+     * @author Peter Heywood <peethwd@gmail.com>
+     */
     public function getUuid(){
         return $this->uuid;
     }
 
+    /**
+     * Getter method for url class variable
+     * @return string
+     * @author Peter Heywood <peethwd@gmail.com>
+     */
     public function getUrl(){
         return $this->url;
     }
 
+    /**
+     * Getter method for river class variable
+     * @return string
+     * @author Peter Heywood <peethwd@gmail.com>
+     */
     public function getRiver(){
         return $this->river;
     }
 
+    /**
+     * Getter method for state class variable
+     * @return string
+     * @author Peter Heywood <peethwd@gmail.com>
+     */
     public function getState(){
         return $this->state;
     }
 
+    /**
+     * Getter method for eaLevel class variable
+     * @return string
+     * @author Peter Heywood <peethwd@gmail.com>
+     */
     public function getEaLevel(){
         return $this->eaLevel;
     }
 
+    /**
+     * Encodes the object into json format for storage.
+     * @return string
+     */
     public function encodeToJson(){
         return json_encode(get_object_vars($this));
     }
+
+    /**
+     * Decode json string into instance of this object.
+     * @param  string $json
+     * @return \IsHppOpen\River
+     */
     public static function decodeFromJson($json){
         $decoded = json_decode($json);
         $obj = new River();
