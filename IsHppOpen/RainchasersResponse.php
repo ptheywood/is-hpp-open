b667d5c86fb47bd4b0a4c9b4c0f9a1155b015388
Peter Heywood <peethwd@gmail.com>
2014-01-10 09:38:41 +0000

    Improve documentation for IsHppOpen/RainchasersResponse

--- IsHppOpen/RainchasersResponse.php
+++ IsHppOpen/RainchasersResponse.php
@@ -10,12 +10,22 @@ class RainchasersResponse {
 
     private $status, $meta, $data, $river, $timeOfResponse;
 
+    /**
+     * Constructor that allows optional automatic processing of JSON
+     * @param string $json string of json from rainchasers
+     * @author  Peter Heywood <peethwd@gmail.com>
+     */
     function __construct($json = null){
         if($json != null){
             $this->processRainchasersRequest($json);
         }
     }
 
+    /**
+     * Process the json string returned by the api, to create a fully populated instance of this class.
+     * @param  string $json
+     * @author  Peter Heywood <peethwd@gmail.com>
+     */
     private function processRainchasersRequest($json){
         if(isset($json["status"])){
             $this->status = $json["status"];
@@ -31,22 +41,47 @@ class RainchasersResponse {
 
     }
 
+    /**
+     * Decide if the returned statis is good or not.
+     * @return boolean
+     * @author  Peter Heywood <peethwd@gmail.com>
+     */
     public function goodStatus(){
         return $this->status == 200;
     }
 
+    /**
+     * Getter method for the data instance variable
+     * @return StdObject
+     * @author  Peter Heywood <peethwd@gmail.com>
+     */
     public function getData(){
         return $this->data;
     }
 
+    /**
+     * Getter method for the river instance variable
+     * @return StdObject
+     * @author  Peter Heywood <peethwd@gmail.com>
+     */
     public function getRiver(){
         return $this->river;
     }
 
+    /**
+     * Getter method for the timeOfResponse instance variable
+     * @return timestamp
+     * @author  Peter Heywood <peethwd@gmail.com>
+     */
     public function getTimeOfResponse(){
         return $this->timeOfResponse;
     }
 
+    /**
+     * Encode the current instance as json for storage.
+     * @return string
+     * @author  Peter Heywood <peethwd@gmail.com>
+     */
     public function encodeToJson(){
         $vars = get_object_vars($this);
         $vars["river"] = json_decode($this->river->encodeToJson());
@@ -54,6 +89,12 @@ class RainchasersResponse {
         return json_encode($vars);
     }
 
+    /**
+     * Decode an instance from stored json. 
+     * @param  string $encodedJson
+     * @return \IsHppOpen\RainchasersResponse
+     * @author  Peter Heywood <peethwd@gmail.com>
+     */
     public static function decodeFromJson($encodedJson){
         $decoded = json_decode($encodedJson);
 
